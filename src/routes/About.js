5bb56f6406d9406204051acf768e5af9e101ba96
kibeom93 <kimgb4916@gmail.com>
2020-07-06 15:57:00 +0900

    movie_app

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { func } from "prop-types";
+
+function About () {
+    return (
+    <div className ="about__container">
+    <span> About this page : I built it because i studied for my project.</span>
+    </div>
+    );
+}
+
+export default About; 
